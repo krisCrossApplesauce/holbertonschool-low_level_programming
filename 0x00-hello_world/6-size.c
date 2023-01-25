0674252a75b579079abaff4bd5df12d0f82d57be
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-25 12:57:00 -0800

    6-size.c: doesnt print #s + doesnt pass betty

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printf
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: ? byte(s)");
+	printf("Size of an int: ? byte(s)");
+	printf("Size of a long int: ? byte(s)");
+	printf("Size of a long long int: ? byte(s)");
+	printf("Size of a float: ? byte(s)");
+	return (0);
+}
