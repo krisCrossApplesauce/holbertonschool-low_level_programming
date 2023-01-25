a97d89ae4dd51e8a36abfab788d853fd405446bb
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-25 13:01:55 -0800

    6-size.c: still doesnt print sizes (confused regarding how)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,16 @@
 
 /**
  * main - printf
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of a char: ? byte(s)");
-	printf("Size of an int: ? byte(s)");
-	printf("Size of a long int: ? byte(s)");
-	printf("Size of a long long int: ? byte(s)");
-	printf("Size of a float: ? byte(s)");
+	printf("Size of a char: ? byte(s)\n");
+	printf("Size of an int: ? byte(s)\n");
+	printf("Size of a long int: ? byte(s)\n");
+	printf("Size of a long long int: ? byte(s)\n");
+	printf("Size of a float: ? byte(s)\n");
 	return (0);
 }
