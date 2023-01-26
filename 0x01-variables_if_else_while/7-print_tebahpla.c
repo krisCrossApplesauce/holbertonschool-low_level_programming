2d3768f0532c4fa801048908b116ab990acfb290
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-26 15:33:23 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	int i;
+
+	char alph[27] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 27; i++)
+	{
+		c = alph[25 - i];
+		if (i < 26)
+		{
+			putchar(c);
+		}
+		else if (i == 26)
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
