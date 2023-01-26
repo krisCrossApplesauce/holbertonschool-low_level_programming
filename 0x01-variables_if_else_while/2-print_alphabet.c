d69b90dafab134c97fb712e721323eca9641495a
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-26 14:19:32 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,92 +8,16 @@
 
 int main(void)
 {
-	char alph;
+	char c;
 
 	int i;
 
+	char alph[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
 	for (i = 0; i < 26; i++)
 	{
-		if (i == 0)
-		{ alph = 'a';
-		}
-		else if (i == 1)
-		{ alph = 'b';
-		}
-		else if (i == 2)
-		{ alph = 'c';
-		}
-		else if (i == 3)
-		{ alph = 'd';
-		}
-		else if (i == 4)
-		{ alph = 'e';
-		}
-		else if (i == 5)
-		{ alph = 'f';
-		}
-		else if (i == 6)
-		{ alph = 'g';
-		}
-		else if (i == 7)
-		{ alph = 'h';
-		}
-		else if (i == 8)
-		{ alph = 'i';
-		}
-		else if (i == 9)
-		{ alph = 'j';
-		}
-		else if (i == 10)
-		{ alph = 'k';
-		}
-		else if (i == 11)
-		{ alph = 'l';
-		}
-		else if (i == 12)
-		{ alph = 'm';
-		}
-		else if (i == 13)
-		{ alph = 'n';
-		}
-		else if (i == 14)
-		{ alph = 'o';
-		}
-		else if (i == 15)
-		{ alph = 'p';
-		}
-		else if (i == 16)
-		{ alph = 'q';
-		}
-		else if (i == 17)
-		{ alph = 'r';
-		}
-		else if (i == 18)
-		{ alph = 's';
-		}
-		else if (i == 19)
-		{ alph = 't';
-		}
-		else if (i == 20)
-		{ alph = 'u';
-		}
-		else if (i == 21)
-		{ alph = 'v';
-		}
-		else if (i == 22)
-		{ alph = 'w';
-		}
-		else if (i == 23)
-		{ alph = 'x';
-		}
-		else if (i == 24)
-		{ alph = 'y';
-		}
-		else if (i == 25)
-		{ alph = 'z';
-		}
-
-		putchar(alph);
+		c = alph[i];
+		putchar(c);
 	}
 	return (0);
 }
