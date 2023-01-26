36260a57d8d8c51325fdd37392867adebc792ade
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-26 15:27:07 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,20 +8,12 @@
 
 int main(void)
 {
-	int d;
-
 	int i;
 
-	int dec[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-	for (i = 0; i < 11; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		d = dec[i];
-		if (i < 10)
-		{
-			putchar(d);
-		}
-		else if (i == 10)
+		putchar(i);
+		if (i == '9')
 		{
 			putchar('\n');
 		}
