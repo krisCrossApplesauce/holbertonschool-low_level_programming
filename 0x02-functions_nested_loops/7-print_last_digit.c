ddf42338efcbad7a4c10350cfd204dfc953068c4
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-27 14:29:41 -0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,7 @@ int print_last_digit(int n)
 {
 	int last;
 
-	if (n % 10)
+	if ((n % 10 ) != 1 && (n % 10) != 2 && (n % 10) != 3 && (n % 10) != 4 && (n % 10) != 5 && (n % 10) != 6 && (n % 10) != 7 && (n % 10) != 8 && (n % 10) != 9)
 	{
 		last = 0;
 	}
@@ -27,5 +27,5 @@ int print_last_digit(int n)
 		last = n % 10;
 	}
 
-	return(last);
+	return (last);
 }
