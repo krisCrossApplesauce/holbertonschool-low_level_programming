57bd709c92bed9447b06603108b7a241f6dc1337
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-27 12:14:50 -0800

    2-main.c & 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - void
+ *
+ * Return: whatever
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	int i, t;
+
+	char alph[27] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (t = 0; t < 10; t++)
+	{
+		for (i = 0; i < 27; i++)
+		{
+			c = alph[i];
+			if (i < 26)
+			{
+				_putchar(c);
+			}
+			else if (i == 26)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
