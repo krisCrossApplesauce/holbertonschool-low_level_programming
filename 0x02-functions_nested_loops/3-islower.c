28009a5685b3fca7a93f37ca02cc327f78b48048
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-27 12:39:52 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - int
  *
- * c - is a character, translated into an int using its ASCII value
+ * @c: is a character, translated into an int using its ASCII value
  * ^that's why c is compared to numbers in if statement, comparing the ASCII values
  *
  * Return: 1 if c is lowercase, 0 otherwise
