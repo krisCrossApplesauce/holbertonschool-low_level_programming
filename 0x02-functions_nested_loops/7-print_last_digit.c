1eb36080e48c0eea289c9756e4f73fb099e5b55b
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-27 13:47:49 -0800

    7-print_last_digit.c & 7-main.c: just like last one, claimed to lack any var, but the last one did need one and passed all checks with one so its just being sneaky

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - does something, that's for sure
+ *
+ * @n: number
+ * ^idc if you say it isnt there, it is
+ *
+ * Return: stuff
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	return (last);
+}
