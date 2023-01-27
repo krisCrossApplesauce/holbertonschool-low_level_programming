bd54af3b3fc468cef630544172f6bc9dbd61e74f
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-27 13:21:55 -0800

    5-sign.c: forgot #include "main.h" & return (0); outside of if statement

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - things happen wow
@@ -13,22 +14,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(0);
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
+
+	return (0);
 }
