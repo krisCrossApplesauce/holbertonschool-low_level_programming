8ec463661117654468219d0b522b7ee43ab8fa2b
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-30 10:30:16 -0800

    9-times_table.c: success i bet

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table (0-9x0-9)
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+
+			if (c < 10)
+			{
+				printf("  %d,", c);
+			}
+			else
+			{
+				printf(" %d,", c);
+			}
+		}
+
+		printf("\n");
+	}
+}
