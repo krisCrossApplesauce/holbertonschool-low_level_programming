c84a50aded12d4917ebea70614eddd8aedfdef1e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-30 10:53:25 -0800

    10-add.c + 10-main.c: should work

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - it adds (gasp, shock, disbelief)
+ *
+ * @a: number 1
+ *
+ * @b: number 2
+ *
+ * Return: the sum of the two numbers
+ */
+
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+
+	return (n);
+}
