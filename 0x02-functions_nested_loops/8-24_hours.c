e78ddf3b66fe7def973527de21a552775e1e821a
krisCrossApplesauce <karisrr2203@gmail.com>
2023-01-30 09:54:29 -0800

    8-24_hours.c & 8-main.c: success?

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - stuff
+ *
+ * Return: times or nothing
+ */
+
+void jack_bauer(void)
+{
+	int m, h;
+
+	for (h = 0; h < 13; h++)
+	{
+		if (h < 10)
+		{
+			printf("0%d:", h);
+		}
+		else
+		{
+			printf("%d:", h);
+		}
+
+		for (m = 0; m < 60; m++)
+		{
+			if (m < 10)
+			{
+				printf("0%d", m);
+			}
+			else
+			{
+				printf("%d", m);
+			}
+		}
+	}
+}
