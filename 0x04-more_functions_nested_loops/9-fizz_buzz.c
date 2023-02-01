67ac135c83c5ab23b4a6838f6da87be4b7f52c4a
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-01 12:23:35 -0800

    9-fizz_buzz.c: please, i dont understand

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,4 +9,32 @@
 
 void fizz_buzz(void)
 {
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i % 15))
+		{
+			printf("FizzBuzz");
+		}
+		else if (!(i % 5))
+		{
+			printf("Buzz");
+		}
+		else if (!(i % 3))
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
 }
