cc1f425b393fd7ec5f3b407f018d7ce8f7665357
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-01 10:27:55 -0800

    3-print_numbers.c & 3-main.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: 0-9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+
+		if (i == 9)
+		{
+			_putchar('\n');
+		}
+	}
+}
