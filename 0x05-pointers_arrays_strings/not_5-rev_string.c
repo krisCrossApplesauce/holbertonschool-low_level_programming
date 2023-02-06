743a79df6a80b8480673a91b489f988fa4023fcd
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-06 12:58:24 -0800

    5-rev_string.c: got help, need to understand, will add comments explaining when figure it out

--- 0x05-pointers_arrays_strings/not_5-rev_string.c
+++ 0x05-pointers_arrays_strings/not_5-rev_string.c
@@ -13,20 +13,41 @@
 void rev_string(char *s)
 {
 	int i = 0;
-
+       
 	int sl = 0;
 
 	char str;
 
+
 	while (s[sl] != '\0')
 	{
+/**
+ * 		if (s[sl + 1] == '\0')
+ * 		{
+ * 			str = s;
+ * 		}
+ */
 		sl++;
 	}
+
+/**
+ * 	for (a = 0; a <= sl; a++)
+ * 	{
+ *
+ * 	str = printf("%s", s);
+ *
+ * 	}
+ * 	printf("str: %s, s: %s \n", str, s);
+ */
 
 	for (i = sl - 1; i >= sl / 2; i--)
 	{
 		str = s[i];
 		s[i] = s[sl - i - 1];
 		s[sl - i - 1] = str;
+/**
+ * 		s[b - 1] = str[sl - b];
+ * 		printf("str: %s, s: %s \n", str, s);
+ */
 	}
 }
