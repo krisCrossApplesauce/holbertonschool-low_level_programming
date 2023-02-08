d29c8ecb892d01890c2b46b37fff9b5b269fab08
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-08 15:06:34 -0800

    6-cap_string.c: \t is tab

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @s: char array/string
+ *
+ * Return: pointer
+ */
+
+char *cap_string(char *s)
+{
+	int i, nw, sl;
+
+	nw = 0;
+
+	sl = 0;
+
+	while (s[sl] != '\0')
+	{
+		sl++;
+	}
+
+	for (i = 0; i < sl; i++)
+	{
+		if (s[i] == 9 || s[i] == 10 || s[i] == 32 || s[i] == 33 || s[i] == 46 || s[i] == 63)
+		{
+			nw = 1;
+		}
+		else 
+		{
+			if ((nw == 1 || i == 0) && s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] = s[i] - 32;
+			}
+
+			nw = 0;
+		}
+	}
+
+	return (s);
+}
