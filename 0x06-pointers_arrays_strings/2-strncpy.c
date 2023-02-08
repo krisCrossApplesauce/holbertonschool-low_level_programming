57b17d05256b2b5e4374dd4ab637829cfba2d1be
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-08 13:07:48 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string to a pointer
+ * ^replacing n amount of the original contents
+ *
+ * @dest: pointer, where string is copied to
+ *
+ * @src: pointer, where string is copied from
+ *
+ * @n: int, amount of char from dest to be replaced by src
+ *
+ * Return: a pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, sl;
+
+	sl = 0;
+
+	while (src[sl] != '\0')
+	{
+		sl++;
+	}
+
+	for (i = 0; i <= sl && i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
