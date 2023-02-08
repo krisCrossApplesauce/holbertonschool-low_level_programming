9dff52e77ac844ad209983565568941a6889c3bb
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-08 15:19:09 -0800

    6-cap_string.c: betty issues

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,11 +24,15 @@ char *cap_string(char *s)
 
 	for (i = 0; i < sl; i++)
 	{
-		if (s[i] == 9 || s[i] == 10 || s[i] == 32 || s[i] == 33 || s[i] == 46 || s[i] == 63)
+		if (s[i] == 9 || s[i] == 10 || s[i] == 32)
 		{
 			nw = 1;
 		}
-		else 
+		else if (s[i] == 33 || s[i] == 46 || s[i] == 63)
+		{
+			nw = 1;
+		}
+		else
 		{
 			if ((nw == 1 || i == 0) && s[i] >= 97 && s[i] <= 122)
 			{
