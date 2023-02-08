8c9d5f43e44c25a049a3de25c42d8bb91e7231c2
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-08 12:29:57 -0800

    0-strcat.c & 0-main.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strcat - combine (concatenates) two strings
+ *
+ * @dest: pointer to first string and destination of combined strings
+ *
+ * @src: pointer to second string
+ *
+ * Return: a pointer(? or a string?)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, sl, dl;
+
+	sl = 0;
+
+	dl = 0;
+
+	while (src[sl] != '\0')
+	{
+		sl++;
+	}
+
+	while (dest[dl] != '\0')
+	{
+		dl++;
+	}
+
+	for (i = 0; i <= sl; i++)
+	{
+		dest[dl] = src[i];
+		dl++;
+	}
+
+	return (dest);
+}
