4726179d23f90bd707c2d287c4ae3a58357496b5
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-11 12:47:29 -0800

    5-strstr.c: betty mad, but i wanna see if it passes the other checks

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: string, where looking for substring
+ *
+ * @needle: substring, what's being looked for
+ *
+ * Return: pointer, beginning of substring in sting
+ * ^or NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b, x, hl, nl;
+
+	x = 0;
+
+	hl = 0;
+
+	nl = 0;
+
+	while (haystack[hl] != '\0')
+	{
+		hl++;
+	}
+
+	while (needle[nl] != '\0')
+	{
+		nl++;
+	}
+
+	for (a = 0; a <= hl && x == 0; a++)
+	{
+		if (haystack[a] == needle[0])
+		{
+			x = 1;
+
+			for (b = 1; b < nl; b++)
+			{
+				if (haystack[a + b] == needle[b])
+				{
+					x++;
+				}
+			}
+
+			if (x == nl)
+			{
+				haystack = &haystack[a];
+			}
+			else
+			{
+				x = 0;
+			}
+		}
+
+		if (a == hl)
+		{
+			haystack = &haystack[hl];
+		}
+	}
+
+	return (haystack);
+}
