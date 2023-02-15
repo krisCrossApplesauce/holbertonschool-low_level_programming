d13be15d047071708054380b57445a11e2b374d8
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-15 12:43:09 -0800

    0-puts_recursion.c: needed if s[0] == "\0" then _putchar("\n"); else (everything else) so that if the given string was empty, it wouldnt print NULL

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,16 +10,14 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(s[0]);
-
-	s = &s[1];
-
-	if (s[0] != '\0')
+	if (s[0] == '\0')
 	{
-		_puts_recursion(s);
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		_putchar(s[0]);
+		s = &s[1];
+		_puts_recursion(s);
 	}
 }
