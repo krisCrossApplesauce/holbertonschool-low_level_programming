7cf5e4c126f360bb1542004271e976aaab19c852
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-16 11:19:43 -0800

    5-sqrt_recursion.c: needed two functions, the function that recurses isnt even _sqrt_recursion, its the other one, kinda annoying

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,8 +9,23 @@
  * Return: int
  */
 
+int _check_recursion(int i, int n)
+{
+	if ((i == n && n != 0 && n != 1) || n < 0)
+	{
+		return (-1);
+	}
+	else if (i * i != n)
+	{
+		return(_check_recursion(i + 1, n));
+	}
+	else
+	{
+		return (i);
+	}
+}
+
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if (n > 0 & 
+	return (_check_recursion(0, n));
+}
