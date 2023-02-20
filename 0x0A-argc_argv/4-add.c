6c9783d455851405b8328e69a0b36b8867f5c3d0
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-20 12:36:18 -0800

    4-add.c: unfinished, cant focus

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - adds positive numbers
+ *
+ * @argc: int
+ *
+ * @argv: array
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, n;
+
+	n = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (*argv[i] >= 0)
+		{
+		}
+	}
+}
