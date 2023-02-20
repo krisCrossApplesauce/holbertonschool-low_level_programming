1350c3162ef3decc7e867f486b8fda82c109446e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-20 10:56:12 -0800

    0-whatsmyname.c: i dont know what im doing bc my brain wont work bc aaaaaaaa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * whats_my_name - prints its name, followed by a new line
+ *
+ * Return: void
+ */
+
+void whats_my_name(char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
+void whats_my_name(char *argv[]);
 
 #endif
