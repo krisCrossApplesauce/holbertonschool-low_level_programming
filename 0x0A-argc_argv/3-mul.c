faacc7559da37bd568f56d60ec0daf4158570eb6
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-20 12:17:41 -0800

    3-mul.c: some checks not passing, taking out if statement to ask if ascii value == number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,26 +14,18 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		if (*argv[1] >= 48 && *argv[1] <= 57 && *argv[2] >= 48 && *argv[2] <= 57)
-		{
-			int a = atoi(argv[1]);
+		int a = atoi(argv[1]);
 
-			int b = atoi(argv[2]);
+		int b = atoi(argv[2]);
 
-			printf("%d\n", a * b);
-			return (0);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		printf("%d\n", a * b);
+		return (0);
 	}
 }
