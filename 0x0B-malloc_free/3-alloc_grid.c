06ccddcc58b98e49c1f13431a0b62292a1e8985c
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-23 17:03:13 -0800

    i dont understand

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: int
+ *
+ * @height: int
+ *
+ * Return: double pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, ii;
+
+	int **grid;
+
+}
