21cd3693a610f40a8a41e4ebc86930b4286432c6
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-26 16:51:13 -0800

    2-calloc.c: the output is correct, no idea about setting the memory to 0 though :T

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ *
+ * @nmemb: unsigned int, number of elements in array
+ *
+ * @size: unsigned int, number of bytes per element in array
+ *
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	a = malloc(nmemb * size);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	nmemb = 0;
+	size = 0;
+
+	return (a);
+}
