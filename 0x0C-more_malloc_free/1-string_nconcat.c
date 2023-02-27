c11ca9f3b4ac9db284c31ca959f63acfb0c3844c
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-27 12:32:37 -0800

    1-string_nconcat.c: shortened to appease betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,10 +22,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL && s2 == NULL)
 	{ return (NULL); }
-
 	if (s1 == NULL)
 	{ s1 = ""; }
-
 	if (s2 == NULL)
 	{ s2 = ""; }
 
@@ -35,10 +33,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1[s1l])
 	{ s1l++; }
-
 	while (s2[s2l])
 	{ s2l++; }
-
 	if (s2l > n)
 	{ s2l = n; }
 
@@ -49,23 +45,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < s1l; i++)
 	{ a[i] = s1[i]; }
-
 	for (ii = 0; ii <= s2l; ii++)
 	{
-		if (n > s2l)
-		{
-			if (ii <= s2l)
-			{ a[ii + s1l] = s2[ii]; }
-			else
-			{ a[ii + s1l] = '\0'; }
-		}
+		if (n > s2l && ii <= s2l)
+		{ a[ii + s1l] = s2[ii]; }
+		else if (n < s2l && ii < s2l)
+		{ a[ii + s1l] = s2[ii]; }
 		else
-		{
-			if (ii < s2l)
-			{ a[ii + s1l] = s2[ii]; }
-			else
-			{ a[ii + s1l] = '\0'; }
-		}
+		{ a[ii + s1l] = '\0'; }
 	}
 
 	return (a);
