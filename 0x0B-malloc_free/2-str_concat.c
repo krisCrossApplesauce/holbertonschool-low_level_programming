94895d39d506aab9ac9641328116338dfc8818f2
krisCrossApplesauce <karisrr2203@gmail.com>
2023-02-27 11:06:01 -0800

    2-str_concat.c: changed back, didnt compile

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,6 +19,11 @@ char *str_concat(char *s1, char *s2)
 
 	char *arr;
 
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -27,11 +32,6 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 	{
 		s2 = "";
-	}
-
-	if (s1 == "" && s2 == "")
-	{
-		return (NULL);
 	}
 
 	s1l = 0;
