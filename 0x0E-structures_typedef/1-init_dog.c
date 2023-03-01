3d858fcac23f785ac75b4f80f656cac5d7c5900a
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-01 15:07:35 -0800

    1-init_dog.c: ok, i see, dog.h is the header file for everything, like main.h, not just the dog struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 
 /**
  * init_dog - initializes a variable of type struct dog
  *
- * @d: ?
+ * @d: struct dog, pointer where dog info is
  * @name: char, name of dog
  * @age: float, age of dog
  * @owner: char, name of owner of dog
@@ -14,4 +14,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
