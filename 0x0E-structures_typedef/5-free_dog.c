f57d9c9ae41c785ece2e4dc59d8bb8e34fd838d0
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-01 15:39:41 -0800

    5-free_dog.c: doesnt work

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs (dog_t/struct dog)
+ *
+ * @d: dog_t pointer, the dog to be freed
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - a dog
  * @name: char, the name of the dog
@@ -21,5 +24,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
