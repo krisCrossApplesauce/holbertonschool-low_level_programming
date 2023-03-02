d4980c622222863e4a67c48a68ee56a1ee36466c
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-02 13:40:40 -0800

    4-new_dog.c: i reverted completely back to where i was prior to trying to fix anything

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,36 +14,12 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nl, ol;
 	dog_t doggo;
 	dog_t *doggo_p;
-
-	if (name == NULL || age < 0 || owner == NULL)
-	{ return (NULL); }
-
-	doggo_p = malloc(sizeof(dog_t));
-
-	if (doggo_p == NULL)
-	{
-		free(doggo_p);
-		return (NULL);
-	}
-
-	while (name[nl])
-	{ nl++; }
-	while (owner[ol])
-	{ ol++; }
 
 	doggo.name = name;
 	doggo.age = age;
 	doggo.owner = owner;
-
-	if (doggo.name == NULL || doggo.owner == NULL)
-	{
-		free(doggo_p);
-		return (NULL);
-	}
-
 	doggo_p = &doggo;
 
 	return (doggo_p);
