75b852049cc6ec8a81e41f7f8fd8e146021b1ed8
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-06 19:26:46 -0800

    3-get_op_func.c: forgot i++ in while loop

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,16 +22,17 @@ int (*get_op_func(char *s))(int, int)
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+		{"%", op_mod}
 	};
 
-	while (i < 6)
+	while (i < 5)
 	{
-		if (s[0] == ops[i].op[0] && i != 5)
+		if (s[0] == ops[i].op[0])
 		{
 			return (ops[i].f);
 		}
+		
+		i++;
 	}
 
 	return (NULL);
