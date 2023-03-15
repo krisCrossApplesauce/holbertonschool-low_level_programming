cc683046491708485675fd1fa52432a52726616e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-15 11:04:51 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  *
@@ -19,5 +22,6 @@ typedef struct list_s
 
 
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
