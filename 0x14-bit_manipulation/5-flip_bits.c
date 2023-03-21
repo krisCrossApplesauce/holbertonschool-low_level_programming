67738038d70ca85cacd25a6ae46909df8cf8f5bf
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-20 21:44:09 -0700

    im rushing bc nearly midnight aaaaaaa

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ * to get from one number to another
+ *
+ * @n: unsigned long int, number
+ * @m: unsigned long int, another number
+ *
+ * Return: unsigned int
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, x;
+
+	for (i = 0; i < (sizeof(n) * 8); i++)
+	{
+		if (((n >> i) & 1) != ((m >> i) & 1))
+		{ x++; }
+	}
+
+	return (x);
+}
