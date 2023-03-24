7dbbdbe97d7a98e2c63077cd182ce57a564bef41
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-24 11:07:51 -0700

    0-hash_table_create.c: does it not need to free stuff when return NULL?

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,18 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *ht = (hash_table_t *) malloc(sizeof(hash_table_t));
 
-	if (ht == NULL)
-	{
-		free(ht);
-		return (NULL);
-	}
+	if (ht == NULL || ht == NULL)
+	{ return (NULL); }
 
 	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (ht->array == NULL)
+	{ return (NULL); }
 
 	for (i = 0; i <= size; i++)
-	{
-		ht->array[i] = NULL;
-	}
+	{ ht->array[i] = NULL; }
 
 	return (ht);
 }
