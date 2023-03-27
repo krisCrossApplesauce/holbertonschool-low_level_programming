35086e226aea9894e78936e58e834be5354a956e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-27 12:03:47 -0700

    3&4: added if (*key == \0) to the if statement that checks for errors and returns (NULL) if any are true

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *new = (hash_node_t *) malloc(sizeof(hash_node_t));
 
-	if (!ht || !key || !new)
+	if (!ht || !key || *key == '\0' || !new)
 	{
 		free(new);
 		return (0);
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *node;
 
-	if (!ht || !key)
+	if (!ht || !key || *key == '\0')
 	{ return (NULL); }
 
 	idx = key_index((const unsigned char *) key, ht->size);
