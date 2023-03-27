9b0a227e8eb096714518b5aae2ef6e4b831bf948
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-27 13:08:29 -0700

    6-hash_table_delete.c: memory leaks, but whatever

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: the hash table to be deleted
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	if (!ht)
+	{ return; }
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			ht->array[i] = ht->array[i]->next;
+
+			free(node->key);
+			free(node->value);
+			free(node);
+		}
+
+		free(ht->array[i]);
+	}
+}
