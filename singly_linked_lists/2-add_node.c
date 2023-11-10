b3b49be837f3a49120e7b328dc1831c868fdd786
root <karisrr2203@gmail.com>
2023-11-10 14:24:17 -0800

    task 2: malloc and free exist (woah)

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of the given singly linked list
+ *
+ * @head: list_t **, head of singly linked list
+ * @str: string for new node of list
+ *
+ * Return: list_t, new head of singly linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+
+	if (head == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
