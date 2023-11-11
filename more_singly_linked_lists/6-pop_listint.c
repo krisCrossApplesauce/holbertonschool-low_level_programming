c55aaeae3e93a8f478817887b4de8a19740fd48b
root <karisrr2203@gmail.com>
2023-11-11 12:08:05 -0800

    task 6 :)

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -3,5 +3,17 @@
 /**
  * pop_listint - deletes the head node of a singly linked list
  *
- * i think im gonna be done for now :P
+ * @head: listint_t **, the head node of a singly linked list
+ *
+ * Return: int, head node's data (n)
  */
+int pop_listint(listint_t **head)
+{
+	listint_t *node = *head;
+	int n = node->n;
+
+	*head = node->next;
+	free(node);
+
+	return (n);
+}
