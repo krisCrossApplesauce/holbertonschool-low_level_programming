1ead2e229925980600946306d8f2977a8d6296b8
root <karisrr2203@gmail.com>
2023-11-10 16:54:47 -0800

    task 0 :)

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements in a singly linked list
+ *
+ * @h: listint_t *, head of singly linked list
+ *
+ * Return: size_t, number of nodes in singly linked list
+ */
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *n = h;
+    size_t c = 0;
+
+    while (n != NULL)
+    {
+        printf("%d\n", n->n);
+        n = n->next;
+        c++;
+    }
+
+    return (c);
+}
--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -20,3 +20,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
+
+size_t print_listint(const listint_t *h);
+
+#endif
