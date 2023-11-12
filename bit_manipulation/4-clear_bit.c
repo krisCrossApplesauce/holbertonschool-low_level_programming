66106a39d9dfb1e27d4faa97780339a8f96f0cb3
root <karisrr2203@gmail.com>
2023-11-12 15:25:44 -0800

    task 4 :D yay! yeah ok I'm definitely starting to understand it again

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at the given index
+ *
+ * @n: unsigned long int *, pointer to the number
+ * @index: unsigned int, the bit to be set to 0
+ *
+ * Return: int, 1 on success, -1 on fail
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(n) * sizeof(unsigned long int)))
+	{ return (-1); }
+
+	*n = *n & ~(0 ^ (1 << index));
+
+	return (1);
+}
