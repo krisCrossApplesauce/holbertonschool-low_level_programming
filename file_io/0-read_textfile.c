50ea4e92704f5eb191ffa68902f428386ea5c223
root <karisrr2203@gmail.com>
2023-11-13 10:34:52 -0800

    task 0: the number of chars it says it's printed isn't what the code example says it should be, but there's no apparent issue with the code so I'm gonna assume it's just my Requiescat file or the code example ,'P but we'll see if the checker agrees with me

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: const char *, name of file
+ * @letters: size_t, number of letters to be read and printed
+ *
+ * Return: ssize_t, number of letters that could be read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, rd, wd;
+	char *buf = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+	{ return (0); }
+
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1)
+	{ return (0); }
+
+	rd = read(fd, buf, letters);
+
+	if (rd == -1)
+	{ return (0); }
+
+	wd = write(STDOUT_FILENO, buf, rd);
+
+	if (wd == -1)
+	{ return (0); }
+
+	close(fd);
+	free(buf);
+	return (wd);
+}
