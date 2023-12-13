8983be8f0050c358534bebd7a6eb39fd2a1bb332
root <karisrr2203@gmail.com>
2023-12-13 13:04:33 -0800

    task 1: ok i figured it out... had to start over

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -4,61 +4,41 @@
  * binary_search - searches for a value in a sorted array of ints
  * using the binary search algorithm
  *
- * @array: int *, the array being searched in
- * @size: size_t, the number of elements in array
+ * @array: int *, the array being searched through
+ * @size: size_t, the number of elements array
  * @value: int, the value being searched for
  *
- * Return: int, the index where value is located (idx),
- * or -1 if value is not in array, or array is NULL
+ * Return: int, the index of the value in the array (idx),
+ * -1 if value isn't in array or if array is NULL
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int s = (int) (size - 1), idx = 0, i;
-	int x = 0, y = 0, a;
+	int max = (int) (size - 1), min = 0;
+	int idx = 0, i;
 
 	if (array == NULL && size > 0)
 	{ return (-1); }
 
-	while (s > 0 && idx < (int) size)
+	while (min <= max)
 	{
-		s += y;
-
-		if (x == 0)
-		{ a = (s - idx); }
-		else
-		{ a = (s + idx); }
+		idx = (min + max) / 2;
 
 		printf("Searching in array: ");
-		for (i = idx; i <= a; i++)
+		for (i = min; i <= max; i++)
 		{
 			printf("%d", array[i]);
-			if (i == a)
+			if (i == max)
 			{ printf("\n"); }
 			else
 			{ printf(", "); }
 		}
 
 		if (value == array[idx])
-		{
-			return (idx);
-		}
-
-		idx += (s / 2);
-
-		if (value <= array[idx])
-		{
-			idx -= (s / 2);
-			x = 0;
-		}
-		else
-		{ x = 1; }
-
-		if (s - (s / 2) == (s / 2) + 1)
-		{ y = 1; }
-		else
-		{ y = 0; }
-		
-		s = (s / 2);
+		{ return (idx); }
+		else if (value < array[idx])
+		{ max = idx - 1; }
+		else if (value > array[idx])
+		{ min = idx + 1; }
 	}
 
 	return (-1);
